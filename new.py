b63dff6e6d26384c81a112b7c9ca2c85727623b5
Bvetsa <79599275+Bvetsa@users.noreply.github.com>
2021-02-24 13:33:57 -0500

    Add files via upload
    added a comment

---
+++ new.py
@@ -0,0 +1,9 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("Echo")
+parser.add_argument("Echius")
+args = parser.parse_args()
+print(args )
+'''
+its a multi line commetn!
+'''
